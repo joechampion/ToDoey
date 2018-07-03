7bb817e2f21f8cffd86418a9b6ebeab406253d7d
joechampion <joechampion@verizon.net>
2018-07-03 17:47:05 -0400

    Persistent local data storage using User Defaults

--- ToDoey/AppDelegate.swift
+++ ToDoey/AppDelegate.swift
@@ -13,9 +13,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+// didFinishLaunchingWithOptions methods gets called FIRST ,when your app starts, BEFORE
+    // viewDidLoad method in the ViewController!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
+        print("didFinishLaunchingWithOptions run!")
+        
         return true
     }
 
@@ -27,6 +34,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        print("applicationDidEnterBackground!")
+        
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -39,6 +49,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        print("applicationWillTerminate!")
+        
     }
 
 
--- ToDoey/ToDoListViewController.swift
+++ ToDoey/ToDoListViewController.swift
@@ -12,10 +12,20 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggos","Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+            
+        }
+        
+        
     }
     //MARK - Tableview Datasource Methods
     
@@ -67,7 +77,7 @@ class ToDoListViewController: UITableViewController {
            // print(textField.text)
             
             self.itemArray.append(textField.text!)
-            
+            self.defaults.set(self.itemArray,forKey: "ToDoListArray")
             self.tableView.reloadData()
             
         }
